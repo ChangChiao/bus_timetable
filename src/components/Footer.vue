687bd484b7c83d8ba53c8d904c84059a51cbb09b
JoeCT_Chang <JoeCT.Chang@moxa.com>
2024-09-03 14:12:13 +0800

    refactor: add year for footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,41 +1,41 @@
 <template>
-    <footer
-        v-if="!isMobile || (isMobile && isIndex)"
-        class="bg-primary-700 h-9 fixed bottom-0 flex justify-center items-center text-white z-50 md:right-0"
-    >
-        The F2E 3rd Week3 - Bus
-    </footer>
+  <footer
+    v-if="!isMobile || (isMobile && isIndex)"
+    class="bg-primary-700 h-9 fixed bottom-0 flex justify-center items-center text-white z-50 md:right-0"
+  >
+    Taiwan Bus © {{ new Date().getFullYear() }}
+  </footer>
 </template>
 
 <script>
 export default {
-  inject: ['appData'],
+  inject: ["appData"],
   computed: {
-    isMobile () {
-      return this.appData?.windowSize < 780
-    }
+    isMobile() {
+      return this.appData?.windowSize < 780;
+    },
   },
-  data () {
+  data() {
     return {
-      isIndex: false
+      isIndex: false,
+    };
+  },
+  mounted() {
+    if (this.$route.path === "/") {
+      this.isIndex = true;
     }
   },
-  mounted () {
-    if (this.$route.path === '/') {
-      this.isIndex = true
-    }
-  }
-}
+};
 </script>
 
 <style lang="postcss">
 footer {
-    border-top-left-radius: 100px;
-    width: calc(100vw - 384px);
+  border-top-left-radius: 100px;
+  width: calc(100vw - 384px);
 }
 @media screen and (max-width: 768px) {
-    footer {
-        width: 100vw;
-    }
+  footer {
+    width: 100vw;
+  }
 }
 </style>
